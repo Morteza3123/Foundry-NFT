e79353b5869ccce1da4c272e920559c8e3d15836
Morteza3123 <mkhedry3123@gmail.com>
2022-11-28 04:02:28 +0330

    first commit

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0 
+pragma solidity ^0.8.10;
+contract SimpleStorage {
+ uint storedData;
+function set(uint x) public {
+  storedData = x;
+ }
+function get() public view returns (uint) {
+  return storedData;
+ }
+}
